2628b836ae3e76740ac98adae4192d6ac73ab269
London Walker <nodnolreklaw@gmail.com>
2018-07-30 10:49:01 -0600

    login field component, text-link component, and more styles

--- src/components/auth/signin-form.js
+++ src/components/auth/signin-form.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
 import { FormTitle } from '../formTitle';
-import { FormInput } from '../formFields';
+import { FormInput, FormButton } from '../formFields';
+import TextLink from '../textLink';
 
 class SigninForm extends Component {
     render() {
         return (
-            <form className='sign-in__form'>
+            <form className='sign-in-form'>
                     <FormTitle className='sign-in-form__title' text='Login'/>
                     <Field className='sign-in-form__email' placeholder='Enter Email' title='Email' component={FormInput} name='email' type='email'/>
                     <Field className='sign-in-form__password' placeholder='Enter Password' title='Password' component={FormInput} name='password' type='password'/>
+                    <Field className='sign-in-form__login'  title='Submit' component={FormButton} name='login' type='button'/>
+                    <div className='sign-in-form__text-links'>
+                    <TextLink to='/forgot' text='Forgot Password'></TextLink>
+                    <TextLink to='/signup' text='Not a member? Register here'></TextLink>
+                    </div>
             </form>
+            
         )
     }
 }
