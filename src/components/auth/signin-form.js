ade4cf780e09272778b8fe90396213fb3189d0c8
London Walker <nodnolreklaw@gmail.com>
2018-07-30 10:15:46 -0600

    form input styles and grid basics completed

--- src/components/auth/signin-form.js
+++ src/components/auth/signin-form.js
@@ -9,7 +9,8 @@ class SigninForm extends Component {
         return (
             <form className='sign-in__form'>
                     <FormTitle className='sign-in-form__title' text='Login'/>
-                    <Field className='sign-in-form__email' component={FormInput} name='email' type='email'/>
+                    <Field className='sign-in-form__email' placeholder='Enter Email' title='Email' component={FormInput} name='email' type='email'/>
+                    <Field className='sign-in-form__password' placeholder='Enter Password' title='Password' component={FormInput} name='password' type='password'/>
             </form>
         )
     }
