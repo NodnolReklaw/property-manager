e8720bd6eac5aaabdbebf79b278a6400ef4c639f
London Walker <nodnolreklaw@gmail.com>
2018-08-06 11:15:32 -0600

    More styling with grid grid-gaps and other stuff

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -7,7 +7,7 @@ class NewsletterLatest extends Component {
                 <h1 className='newsletter-latest__title'> Title </h1>
                 <img className='nesletter-latest__image' src='http://via.placeholder.com/960x258'/>
                 <div className='newsletter-latest__body'>
-                    <p>Lorem Ipsum</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget pellentesque felis. Phasellus dapibus mollis enim, ac porttitor dolor elementum vitae. Donec vel nisi sit amet ligula vestibulum venenatis. Fusce fermentum massa sit amet metus condimentum laoreet. In egestas, nisi et dapibus malesuada, nunc eros rhoncus libero, in pretium velit tortor vitae lorem. Aenean nec nisi dignissim odio imperdiet convallis. Sed mi ligula, varius eget aliquam sit amet, egestas nec elit. In nec turpis tempus, accumsan risus id, bibendum nisl. Aliquam erat volutpat. Etiam egestas varius nisl sed ornare. Quisque rutrum a erat id facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium molestie varius. Nulla elementum euismod lectus ac vehicula.</p>
                 </div>
             </div>
         )
