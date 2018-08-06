2105d2e32518e2dc3289ebdaacb802516941b1cb
London Walker <nodnolreklaw@gmail.com>
2018-08-06 08:46:48 -0600

    built newsletter box and date

--- src/components/newsletter/newsletterGrid.js
+++ src/components/newsletter/newsletterGrid.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import NewsletterBox from './newsletterBox';
 
 class NewsletterGrid extends Component {
     render() {
         return (
             <div className='newsletter-grid'>
-asdfadsfadsf
+                {/*add button*/}
+                <NewsletterBox date={new Date()}/>
+                {/*archive*/}
+                {/*content*/}
             </div>
         )
     }
