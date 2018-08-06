229cdd661b29422661fb5c51304ad8ad6e1118f6
London Walker <nodnolreklaw@gmail.com>
2018-08-06 11:26:38 -0600

    mapped the newsletter stuff with newsletter latest and set up for future back end use

--- src/components/newsletter/newsletterLatest.js
+++ src/components/newsletter/newsletterLatest.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 class NewsletterLatest extends Component {
     render() {
+        const { title, imageUrl, body } = this.props;
         return (
             <div className='newsletter-latest'>
-                <h1 className='newsletter-latest__title'> Title </h1>
-                <img className='nesletter-latest__image' src='http://via.placeholder.com/960x258'/>
+                <h1 className='newsletter-latest__title'>{title}</h1>
+                <img className='nesletter-latest__image' src={imageUrl}/>
                 <div className='newsletter-latest__body'>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget pellentesque felis. Phasellus dapibus mollis enim, ac porttitor dolor elementum vitae. Donec vel nisi sit amet ligula vestibulum venenatis. Fusce fermentum massa sit amet metus condimentum laoreet. In egestas, nisi et dapibus malesuada, nunc eros rhoncus libero, in pretium velit tortor vitae lorem. Aenean nec nisi dignissim odio imperdiet convallis. Sed mi ligula, varius eget aliquam sit amet, egestas nec elit. In nec turpis tempus, accumsan risus id, bibendum nisl. Aliquam erat volutpat. Etiam egestas varius nisl sed ornare. Quisque rutrum a erat id facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pretium molestie varius. Nulla elementum euismod lectus ac vehicula.</p>
+                    <p>{body}</p>
                 </div>
             </div>
         )
